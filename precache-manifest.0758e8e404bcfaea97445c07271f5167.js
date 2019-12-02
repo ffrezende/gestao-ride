21229c935ef68ecf8f316fe0aa46646b75cd9595
ffrezende <fabriciofraga15@gmail.com>
2019-12-02 17:35:17 -0300

    Updates

---
+++ precache-manifest.0758e8e404bcfaea97445c07271f5167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecb45d1e15a5615ea7bd212e3640c08",
+    "url": "/gestao-ride/index.html"
+  },
+  {
+    "revision": "82f9c6cf662c758d8d8c",
+    "url": "/gestao-ride/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3c4f4cf4c4c7b3103c13",
+    "url": "/gestao-ride/static/js/2.4590bfa1.chunk.js"
+  },
+  {
+    "revision": "82f9c6cf662c758d8d8c",
+    "url": "/gestao-ride/static/js/main.581645e1.chunk.js"
+  },
+  {
+    "revision": "8dd73849eb964ddbfc54",
+    "url": "/gestao-ride/static/js/runtime~main.67919a98.js"
+  }
+]);
